cbc6289d1cce51c02b9142c46ef8b57ed27f1448
elvesbd <elvesbd@yahoo.com>
2023-07-18 14:38:27 -0300

    chore: add files no tracking

---
+++ VehicleCatalogAPI/Migrations/20230718145741_InitialCreation.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleCatalogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "VARCHAR(80)", maxLength: 80, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR(160)", maxLength: 160, nullable: false),
+                    CellPhone = table.Column<string>(type: "VARCHAR(80)", maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "VARCHAR(80)", maxLength: 80, nullable: false),
+                    Brand = table.Column<string>(type: "VARCHAR(80)", maxLength: 80, nullable: false),
+                    Model = table.Column<string>(type: "VARCHAR(80)", maxLength: 80, nullable: false),
+                    Image = table.Column<string>(type: "VARCHAR(2000)", maxLength: 2000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
