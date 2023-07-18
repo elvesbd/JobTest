73a974254944efeb120bbfcbd749c9d678423e6d
elvesbd <elvesbd@yahoo.com>
2023-07-18 11:04:46 -0300

    feat: configured dependency injection and database connection

--- VehicleCatalogAPI/Program.cs
+++ VehicleCatalogAPI/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using VehicleCatalogAPI.Data;
+using VehicleCatalogAPI.Repositories;
+using VehicleCatalogAPI.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddEntityFrameworkSqlServer()
+    .AddDbContext<VehicleCatalogDbContext>(
+        options => options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
