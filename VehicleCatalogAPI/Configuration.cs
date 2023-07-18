180e982ef7ca4bf621b12f88df452d76fd04cae6
elvesbd <elvesbd@yahoo.com>
2023-07-18 15:56:27 -0300

    feat: add configuration for generate token

---
+++ VehicleCatalogAPI/Configuration.cs
@@ -0,0 +1,6 @@
+namespace VehicleCatalogAPI;
+
+public static class Configuration
+{
+    public static string JwtKey { get; set; } = "jQ7OgWOKnMIH8Glc8akbgPLQwE568Js74Dxqi9c=";
+}
---
+++ VehicleCatalogAPI/Services/TokenService.cs
@@ -0,0 +1,25 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using VehicleCatalogAPI.Domain.Models;
+
+namespace VehicleCatalogAPI.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature
+            )
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
