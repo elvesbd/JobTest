a643515fbde5b38c4145d9c4e20fe8cb804ca3da
elvesbd <elvesbd@yahoo.com>
2023-07-18 17:04:34 -0300

    feat: add configuring authentication

--- VehicleCatalogAPI/Extensions/AppExtensions.cs
+++ VehicleCatalogAPI/Extensions/AppExtensions.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using VehicleCatalogAPI.Data;
 using VehicleCatalogAPI.Repositories;
 using VehicleCatalogAPI.Repositories.Interfaces;
@@ -8,6 +11,26 @@ namespace VehicleCatalogAPI.Extensions;
 
 public static class AppExtensions
 {
+    public static void ConfigureAuthentication(this WebApplicationBuilder builder)
+    {
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+
+        builder.Services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+    }
+
     public static void ConfigureMvc(this WebApplicationBuilder builder)
     {
         builder.Services
@@ -34,7 +57,7 @@ public static class AppExtensions
     {
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
         builder.Services.AddDbContext<VehicleCatalogDbContext>(options => options.UseSqlServer(connectionString));
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
-        builder.Services.AddScoped<UserService>();
+        builder.Services.AddTransient<IUserRepository, UserRepository>();
+        builder.Services.AddTransient<UserService>();
     }
 }
