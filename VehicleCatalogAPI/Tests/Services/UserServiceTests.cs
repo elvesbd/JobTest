3a587689bcd3d3eb125bc15ee55996fc1e073793
elvesbd <elvesbd@yahoo.com>
2023-07-24 19:09:24 -0300

    chore: add setup config for test files

---
+++ VehicleCatalogAPI/Tests/Services/UserServiceTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class CreateOrderCommandTests
+{
+    [TestMethod]
+    [TestCategory("Services")]
+    public void Ensure()
+    {
+        Assert.Fail();
+    }
+}
