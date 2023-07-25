39566e6c72bc8365ecc5c1d47a7060a0939b2fb4
elvesbd <elvesbd@yahoo.com>
2023-07-24 21:32:43 -0300

    test: ensure to create a user on success

--- VehicleCatalogAPI/Tests/Services/UserServiceTests.cs
+++ VehicleCatalogAPI/Tests/Services/UserServiceTests.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using VehicleCatalogAPI.Domain.Models;
@@ -16,8 +17,8 @@ public class UserServiceTests
     {
         Name = "John Doe",
         Email = "john.doe@example.com",
-        CellPhone = "1234567890",
-        Password = "password"
+        CellPhone = "85999491515",
+        Password = "12345678"
     };
 
     [TestInitialize]
@@ -45,6 +46,29 @@ public class UserServiceTests
         );
 
         var result = await userService.GetByEmailAsync(_dto.Email);
+        Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+    [TestCategory("Services")]
+    public async Task Ensures_that_a_user_is_successfully_added()
+    {
+        var mockHashed = "any_hash";
+        Debug.Assert(mockHashed == "any_hash");
+        passwordHasherMock?.Setup(x => x.Hash(_dto.Password)).Returns(
+          mockHashed
+       );
+        var mockUser = new User
+        (
+            "Elvis Presley",
+            "elvesp@mail.com",
+            "85999491212",
+            mockHashed
+        );
+
+        repositoryMock?.Setup(x => x.AddAsync(It.IsAny<User>())).ReturnsAsync(mockUser);
+
+        var result = await userService.AddAsync(_dto);
         Assert.IsNotNull(result);
     }
 }
