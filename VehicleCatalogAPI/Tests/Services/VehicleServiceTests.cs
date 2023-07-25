d6371fcfd1c1f843204b373769f1e2c59dcfbcc0
elvesbd <elvesbd@yahoo.com>
2023-07-25 10:32:09 -0300

    test: ensure to returns an exception if vehicle not found

--- VehicleCatalogAPI/Tests/Services/VehicleServiceTests.cs
+++ VehicleCatalogAPI/Tests/Services/VehicleServiceTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using VehicleCatalogAPI.Domain.Models;
 using VehicleCatalogAPI.DTOs.User;
+using VehicleCatalogAPI.Exceptions;
 using VehicleCatalogAPI.Models.Repositories;
 using VehicleCatalogAPI.Services;
 
@@ -11,6 +12,7 @@ public class VehicleServiceTests
     private Mock<IVehicleRepository>? _repositoryMock;
     private VehicleService? _vehicleService;
     private readonly Guid _userId = new Guid("8b0b1d6f-e875-4879-90ec-484e6f14bc78");
+    private readonly Guid _Id = new Guid("ac0b1d6f-e875-4879-90ec-484e6f14bc99");
     private readonly VehicleDto _dto = new VehicleDto
     {
         Name = "Hilux",
@@ -87,5 +89,41 @@ public class VehicleServiceTests
         Assert.IsNotNull(result);
         Assert.AreEqual(_userId, result?.FirstOrDefault()?.UserId);
         Assert.AreEqual(_vehicles, result);
+    }
+
+    [TestMethod]
+    [TestCategory("Services")]
+    public async Task Ensures_to_returns_a_empty_list_if_not_exists_vehicles_of_the_informed_user()
+    {
+        _repositoryMock?.Setup(x => x.GetByUserIdAsync(_userId)).ReturnsAsync(new List<Vehicle>());
+
+        var result = await _vehicleService.GetByUserIdAsync(_userId);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(0, result.Count);
+    }
+
+    [TestMethod]
+    [TestCategory("Services")]
+    public async Task Ensures_to_return_a_vehicle_by_id()
+    {
+        _repositoryMock?.Setup(x => x.GetOneAsync(_vehicle.Id)).ReturnsAsync(_vehicle);
+
+        var result = await _vehicleService.GetOneAsync(_vehicle.Id);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result, _vehicle);
+    }
+
+    [TestMethod]
+    [TestCategory("Services")]
+    public async Task Ensures_to_not_return_a_vehicle_by_id()
+    {
+        _repositoryMock?.Setup(x => x.GetOneAsync(_Id)).ReturnsAsync((Vehicle)null);
+
+        var result = await Assert.ThrowsExceptionAsync<VehicleNotFoundException>
+        (
+            async () => await _vehicleService.GetOneAsync(_Id)
+        );
     }
 }
